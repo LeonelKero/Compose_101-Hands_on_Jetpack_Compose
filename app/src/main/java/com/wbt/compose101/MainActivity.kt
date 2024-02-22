8cdad06fe72af71ed76743e9f3e085d5a9be8373
leonel ka <waboleonel@gmail.com>
2024-02-22 19:36:53 +0100

    Preview composable with i18n

--- app/src/main/java/com/wbt/compose101/MainActivity.kt
+++ app/src/main/java/com/wbt/compose101/MainActivity.kt
@@ -3,13 +3,29 @@ package com.wbt.compose101
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wbt.compose101.ui.theme.Compose101Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +34,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Compose101Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
                 }
             }
         }
@@ -27,17 +46,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    val name = remember { mutableStateOf("") }
+    val isNameEntered = remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        if (isNameEntered.value) {
+            Greet(name.value)
+        } else {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.welcome_message),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.labelLarge
+                )
+                TextAndButton(name, isNameEntered)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greet(name: String) {
+    Text(text = "Hello $name")
+}
+
+@Composable
+fun TextAndButton(name: MutableState<String>, isNameEntered: MutableState<Boolean>) {
+    Row(modifier = Modifier.padding(top = 4.dp)) {
+        OutlinedTextField(
+            value = name.value,
+            onValueChange = {
+                name.value = it
+            },
+            placeholder = { Text(text = stringResource(id = R.string.hint)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
+                capitalization = KeyboardCapitalization.Words
+            ),
+            keyboardActions = KeyboardActions(onAny = {
+                isNameEntered.value = true
+            })
+        )
+        Button(onClick = { isNameEntered.value = true }) {
+            Text(
+                text = stringResource(id = R.string.greet),
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, locale = "fr")
 @Composable
 fun GreetingPreview() {
     Compose101Theme {
-        Greeting("Android")
+        Greeting()
     }
 }
